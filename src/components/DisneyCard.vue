8e7b115e822e7e738458f64417390aec63d6566c
juanjnunez <juanjnunez@yahoo.com>
2023-12-17 01:10:24 -0500

    public api complete

--- src/components/DisneyCard.vue
+++ src/components/DisneyCard.vue
@@ -1,10 +1,26 @@
 <template>
     <div>
-        {{ card.name }}
-        <img :src="card.imageUrl" alt="">
-        <div v-if="card.videoGames && card.videoGames.length > 0">
-            <p>Video Games</p>
-            {{ card.videoGames.join(', ') }}
+        <div id="character_card">
+            <div id="character_card_details">
+                <h1>{{ card.name }}</h1>
+                <img :src="card.imageUrl" alt="">
+                <div v-if="card.films && card.films.length > 0">
+                    <h2>Film Career</h2>
+                    <p>{{ card.films.join(', ') }}</p>
+                </div>
+                <div v-if="card.tvShows && card.tvShows.length > 0">
+                    <h2>TV Career</h2>
+                    <p>{{ card.tvShows.join(', ') }}</p>
+                </div>
+                <div v-if="card.videoGames && card.videoGames.length > 0">
+                    <h2>Video Games</h2>
+                    <p>{{ card.videoGames.join(', ') }}</p>
+                </div>
+                <div v-if="card.parkAttractions && card.parkAttractions.length > 0">
+                    <h2>Park Attractions</h2>
+                    <p> {{ card.parkAttractions.join(', ') }}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -12,15 +28,9 @@
 <script>
 export default {
     name: 'disneyCard',
-    props: ['card'],
-    data() {
-        return {
-            // data properties go here
-        };
+    props: {
+        card: Object
     },
-    methods: {
-        // methods go here
-    }
 };
 </script>
 
--- src/components/DisneyList.vue
+++ src/components/DisneyList.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <disneyCard v-for="card in cards" :key="card._id" :card="card"/>
+    <div v-if="isLoading" class="spinner"></div>
+    <div v-else>
+      <disneyCard :key="currentCard._id" :card="currentCard" />
+      <button class="button" @click="previousCharacter" :disabled="counter <= 0">Previous</button>
+      <button class="button" @click="nextCharacter" :disabled="counter >= cards.length - 1">Next</button>
+    </div>
   </div>
 </template>
 
@@ -14,24 +19,58 @@ export default {
   data() {
     return {
       cards: [],
+      isLoading: true,
+      counter: 0
     };
   },
 
-  created(){
+  created() {
     disneyService.getCharacters()
-    .then((response) => {
-      this.cards = response.data.data;
-    })
+      .then((response) => {
+        this.cards = response.data.data;
+        this.isLoading = false;
+      })
+      .catch(error => {
+        console.error('Error fetching characters:', error);
+        this.isLoading = false;
+      });
   },
 
-  components : { disneyCard },
+  components: { disneyCard },
 
   methods: {
-    // methods go here
+    nextCharacter() {
+      if (this.counter < this.cards.length - 1) {
+        this.counter++;
+      }
+    },
+    previousCharacter() {
+      if (this.counter > 0) {
+        this.counter--;
+      }
+    }
+  },
+
+  computed: {
+    currentCard() {
+      if (this.counter >= 0 && this.counter < this.cards.length) {
+        return this.cards[this.counter];
+      }
+      return null;
+    }
   }
+
 };
 </script>
 
 <style scoped>
-/* CSS for your component goes here */
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
