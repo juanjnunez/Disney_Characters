d594889823ebb9305c34deb12bba2568f56313fc
juanjnunez <juanjnunez@yahoo.com>
2023-12-18 00:23:32 -0500

    Added card expanding feature and styling

--- src/components/DisneyCard.vue
+++ src/components/DisneyCard.vue
@@ -1,22 +1,26 @@
 <template>
     <div>
-        <div id="character_card">
-            <div id="character_card_details">
+        <div id="character-card">
+            <div id="character-card-details">
                 <h1>{{ card.name }}</h1>
-                <img :src="card.imageUrl" alt="">
-                <div v-if="card.films && card.films.length > 0">
+                <img class="image" :src="card.imageUrl" alt="">
+                <div class="film" v-if="card.films && card.films.length > 0">
+                    <img class="icon-img" src="../assets/film.png"/>
                     <h2>Film Career</h2>
                     <p>{{ card.films.join(', ') }}</p>
                 </div>
-                <div v-if="card.tvShows && card.tvShows.length > 0">
+                <div class="tv" v-if="card.tvShows && card.tvShows.length > 0">
+                    <img class="icon-img" src="../assets/tv icon.png"/>
                     <h2>TV Career</h2>
                     <p>{{ card.tvShows.join(', ') }}</p>
                 </div>
-                <div v-if="card.videoGames && card.videoGames.length > 0">
+                <div class="games" v-if="card.videoGames && card.videoGames.length > 0">
+                    <img class="icon-img" src="../assets/games.png"/>
                     <h2>Video Games</h2>
                     <p>{{ card.videoGames.join(', ') }}</p>
                 </div>
-                <div v-if="card.parkAttractions && card.parkAttractions.length > 0">
+                <div class="park" v-if="card.parkAttractions && card.parkAttractions.length > 0">
+                    <img class="icon-img" src="../assets/park.png">
                     <h2>Park Attractions</h2>
                     <p> {{ card.parkAttractions.join(', ') }}</p>
                 </div>
@@ -35,5 +39,14 @@ export default {
 </script>
 
 <style scoped>
-/* CSS for your component goes here */
+.image{
+    border-radius: 3px;
+    margin-bottom: 10px;
+}
+
+.icon-img{
+    margin-top: 10px;
+    max-width: 40px;
+}
+
 </style>
--- src/components/DisneyList.vue
+++ src/components/DisneyList.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     <div v-if="isLoading" class="spinner"></div>
-    <div v-else>
-      <disneyCard :key="currentCard._id" :card="currentCard" />
-      <button class="button" @click="previousCharacter" :disabled="counter <= 0">Previous</button>
-      <button class="button" @click="nextCharacter" :disabled="counter >= cards.length - 1">Next</button>
+    <div class="card" v-else>
+      <div class="card-header">
+        <button class="button-icon previous" @click="previousCharacter" :disabled="counter <= 0">Previous</button>
+        <button class="button-icon next" @click="nextCharacter" :disabled="counter >= cards.length - 1">Next</button>
+      </div>
+      <div class="card-content" :class="{ 'expanded': expanded }">
+        <disneyCard :key="currentCard._id" :card="currentCard" />
+        <button class="see-more" @click="toggleExpand">{{ expanded ? 'See less ↑' : 'See more ↓' }}</button>
+      </div>
     </div>
   </div>
 </template>
@@ -20,7 +25,8 @@ export default {
     return {
       cards: [],
       isLoading: true,
-      counter: 0
+      counter: 0,
+      expanded: false
     };
   },
 
@@ -48,6 +54,10 @@ export default {
       if (this.counter > 0) {
         this.counter--;
       }
+    },
+
+    toggleExpand() {
+      this.expanded = !this.expanded;
     }
   },
 
@@ -64,6 +74,92 @@ export default {
 </script>
 
 <style scoped>
+.card {
+  background: white;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  position: relative;
+  max-width: 350px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.card-header {
+  display: flex;
+  justify-content: flex-end;
+  padding: 10px;
+}
+
+.button-icon {
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 5px 10px;
+  font-size: 16px;
+}
+
+.button-icon:hover {
+  background-color: #ddd;
+}
+
+.see-more {
+  z-index: 2;
+  background: #9696f5;
+  color: white;
+  border: none;
+  border-radius: 30px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  padding: 10px 15px;
+  text-decoration: none;
+  display: inline-block;
+  cursor: pointer;
+  transition: background-color 0.3s, box-shadow 0.3s;
+  position: absolute;
+  bottom: 0;
+  transform: translateX(-50%);
+  margin-bottom: 10px;
+}
+
+.see-more:hover,
+.see-more:focus {
+  background-color: #7a7aeb;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+  outline: none;
+}
+
+/* Initial state */
+.card-content {
+  padding: 20px;
+  position: relative;
+  max-height: 400px;
+  overflow: hidden;
+  transition: max-height 0.3s ease;
+}
+
+.card-content::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 20%; 
+    background: linear-gradient(to top, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
+    z-index: 1; 
+}
+
+.card-content.expanded {
+  padding-bottom: 60px;
+  max-height: 100%;
+}
+
+.card-content.expanded::before {
+    opacity: 0; 
+}
+
 @keyframes spin {
   0% {
     transform: rotate(0deg);
